af99c5c578d2dc3753db5172b68539bf1c8e4dda
Min-su-Jeong <minsu1255@naver.com>
2025-10-09 17:43:09 +0900

    feat: Entity 생성

---
+++ src/main/java/com/kakao_tech_bootcamp/community/entity/PostLike.java
@@ -0,0 +1,31 @@
+package com.kakao_tech_bootcamp.community.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_like")
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class PostLike {
+
+    @Id
+    private PostLikeId id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public PostLike(Integer userId, Integer postId) {
+        this.id = new PostLikeId(userId, postId);
+    }
+}
---
+++ src/main/java/com/kakao_tech_bootcamp/community/entity/User.java
@@ -0,0 +1,56 @@
+package com.kakao_tech_bootcamp.community.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false, unique = true, length = 20)
+    private String nickname;
+
+    @Column(name = "profile_image_url", length = 2048)
+    private String profileImageUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public User(String email, String password, String nickname, String profileImageUrl) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
